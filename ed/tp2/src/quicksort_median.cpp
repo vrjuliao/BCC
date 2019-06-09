4e5414d5367effefaa6f28c65b89eb9eeeb2d890
Vinicius Julião <vrjuliao@Viniciuss-MacBook-Air.local>
2019-06-09 18:12:25 -0300

    fix QM3 comparisons

--- ed/tp2/src/quicksort_median.cpp
+++ ed/tp2/src/quicksort_median.cpp
@@ -4,8 +4,10 @@ QuickSort_Median::QuickSort_Median():QuickSort(){}
 unsigned int QuickSort_Median::getting_pivot(unsigned int vector[],int i, int j){
     int central_index = (i+j)/2;
     unsigned int median = vector[i];
+    mComparisons+=4;
     if((median < vector[central_index] && median < vector[j])
     || (median > vector[central_index] && median > vector[j])){
+        mComparisons+=4;
         median = vector[j];
         if((median < vector[central_index] && median < vector[i])
         || (median > vector[central_index] && median > vector[i])){
