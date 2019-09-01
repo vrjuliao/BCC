36545209387ebc42a2925dd368bce4cdcc089bcb
Vinicius Julião <vrjuliao@hotmail.com>
2019-09-01 16:17:41 -0300

    set indentation

--- organizacao-de-computadores-1/tp1/ex1.asm
+++ organizacao-de-computadores-1/tp1/ex1.asm
@@ -1,7 +1,7 @@
 # x12 register contains the function parameter
 # the value that needs verifications should be putted on X12 register
 IS_ODD:
-	addi x5, x0, 1
-	and a1, x12, x5 
-    addi a0, x0, 1 # print interger
+    andi x11, x12, 1
+    # print interger
+    addi x10, x0, 1
     ecall
--- organizacao-de-computadores-1/tp1/ex2.asm
+++ organizacao-de-computadores-1/tp1/ex2.asm
@@ -1,25 +1,30 @@
 # x12 register contains the function parameter
 FACTORIAL:
-	#grant persistent data in registers
+    # grant persistent data in registers
     addi sp, sp, -8
     sw x5, 0(sp)
     sw x6, 4(sp)
-    
+
     add x11, x0, x0 
-    blt x12, x0, END #return 0 if parameter is less than zero
-    
+
+    # return 0 if parameter is less than zero
+    blt x12, x0, END
+
     addi x11, x0, 1
-    beq x12, x0, END #return 1 if parameter is equals zero
+    # return 1 if parameter is equals zero
+    beq x12, x0, END
+    
     addi x6, x12, 1
     addi x5, x0, 1
+    
     LOOP:
-    	
-    	mul x11, x11, x5
+        mul x11, x11, x5
         addi x5, x5, 1
         blt x5, x6, LOOP
-        END:
-
-    addi a0, x0, 1 # print interger
+    
+    END:
+    # print interger
+    addi a0, x0, 1
     ecall
     
     lw x6, 4(sp)
