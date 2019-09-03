46b1f93244c1820a3dd34f04e60d9e9817b7a9aa
Vinicius Julião <vrjuliao@hotmail.com>
2019-09-03 09:02:01 -0300

    fixing ex3 and fix indentation

--- organizacao-de-computadores-1/tp1/ex1.asm
+++ organizacao-de-computadores-1/tp1/ex1.asm
@@ -1,7 +1,19 @@
+.data
+    input:.word 7 # User input
+
+.text
+main:
+    la x5, input
+    lw x12, 0(x5)
+    jal x1, IS_ODD
+    addi a1, x10, 0 # a1 will stores the result
+    addi a0, x0, 1 # Making ecall be the output
+    ecall # Printing the result
+    jal zero, END
+
 # x12 register contains the function parameter
 # the value that needs verifications should be putted on X12 register
 IS_ODD:
-    andi x11, x12, 1
-    # print interger
-    addi x10, x0, 1
-    ecall
+    andi x10, x12, 1
+    jalr x0, 0(x1)
+END:
--- organizacao-de-computadores-1/tp1/ex2.asm
+++ organizacao-de-computadores-1/tp1/ex2.asm
@@ -1,3 +1,16 @@
+.data
+    input:.word 7 # User input
+
+.text
+main:
+    la x5, input
+    lw x12, 0(x5)
+    jal x1, FACTORIAL
+    addi a1, x10, 0 # a1 will stores the result
+    addi a0, x0, 1 # Making ecall be the output
+    ecall # Printing the result
+    jal zero, END
+
 # x12 register contains the function parameter
 FACTORIAL:
     # grant persistent data in registers
@@ -5,28 +18,27 @@ FACTORIAL:
     sw x5, 0(sp)
     sw x6, 4(sp)
 
-    add x11, x0, x0 
+    add x10, x0, x0 
 
     # return 0 if parameter is less than zero
-    blt x12, x0, END
+    blt x12, x0, END_FACTORIAL
 
-    addi x11, x0, 1
+    addi x10, x0, 1
     # return 1 if parameter is equals zero
-    beq x12, x0, END
+    beq x12, x0, END_FACTORIAL
     
     addi x6, x12, 1
     addi x5, x0, 1
     
     LOOP:
-        mul x11, x11, x5
+        mul x10, x10, x5
         addi x5, x5, 1
         blt x5, x6, LOOP
     
-    END:
-    # print interger
-    addi a0, x0, 1
-    ecall
-    
+END_FACTORIAL:
     lw x6, 4(sp)
     lw x5, 0(sp)
     addi sp, sp, 8
+    jalr x0, 0(x1)
+
+END:
