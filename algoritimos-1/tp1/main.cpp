753e7f0148818f38387c00bf902352d2671c6931
Vinicius Julião <vrjuliao@hotmail.com>
2019-09-23 10:46:31 -0300

    adding time manipulators

--- algoritimos-1/tp1/main.cpp
+++ algoritimos-1/tp1/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
 #include <chrono>
 
 #include "graph.hpp"
 
-int main(){
+int main(int argc, const char *argv[]){
+    
+    //getting parameter whether time must be show
+    bool show_time = false;
+    if(argc > 1){
+        std::string param = "-t";
+        if(argv[1] == param){
+            show_time = true;
+        }
+    }
+
     //time manipulators
     std::chrono::high_resolution_clock::time_point t1;
     std::chrono::high_resolution_clock::time_point t2;
@@ -12,7 +23,6 @@ int main(){
 
     //init time
     t1 = std::chrono::high_resolution_clock::now();
-    //sort vector
     
     //end time
     t2 = std::chrono::high_resolution_clock::now();
@@ -48,15 +58,27 @@ int main(){
         switch(instruction){
             case 'C':
                 std::cin >> value1;
+                if(show_time)
+                    t1 = std::chrono::high_resolution_clock::now();
                 graph.commander(value1);
                 break;
             case 'M':
+                if(show_time)
+                    t1 = std::chrono::high_resolution_clock::now();
                 graph.meeting();
                 break;
             case 'S':
                 std::cin >> value1 >> value2;
+                if(show_time)
+                    t1 = std::chrono::high_resolution_clock::now();
                 graph.swap(value1, value2);
                 break;
+        }
+        if(show_time){
+            t2 = std::chrono::high_resolution_clock::now();
+            elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            std::cout << "Time in nanoseconds: " << static_cast<unsigned int>(elapsed_time.count());
+            std::cout << std::endl << std::endl;
         }
     }
     return 0;
