ee24c0e5dd409637b55c78345c238e1fd6825679
Vinicius Juliao <vrjuliao@hotmail.com>
2019-11-14 18:50:27 -0300

    adding NOPs at asm code

--- organizacao-de-computadores-1/tp2/ex1.asm
+++ organizacao-de-computadores-1/tp2/ex1.asm
@@ -6,19 +6,23 @@ array_size: .word 1
 main:
 la x8, array
 la x5, array_size
-lw x6, 0(x5) #N
-addi x9, x8, 0 # x = &array[0]
+lw x6, 0(x5) # N
+addi x9, x8, 0 #x = &array[0]
 addi x10, x0, 0 # total = 0
 addi x11, x0, 0 # i = 0
 loop:
     lw x12, 0(x9) # x12 = array[i]
-    add x10, x10,x12 # sum += array[i]
+    nop #No Operation
+    add x10, x10, x12 # sum += array[i]
     addi x9, x9, 4 #&array[i++]
     addi x11, x11, 1 # i++
-    blt x11 , x6 , loop
+    nop #No Operation
+    nop #No Operation
+    blt x11, x6, loop
+    nop #No Operation before return to 'loop' caused by branch wrong prevision
 
-addi a1 , x10 , 0 # a1 will stores the result
-addi a0 , x0 , 1 # Making ecall be the output
+addi a1, x10, 0 #a1 will stores the result
+addi a0, x0, 1 #Making ecall be the output
 ecall # Printing the result
 jal zero, end
 end:
