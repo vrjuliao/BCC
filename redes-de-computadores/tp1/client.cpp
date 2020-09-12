fef6709b4fee068e41d78fb8d86def9baaee76b3
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-09-11 22:22:56 -0300

    redes(tp1): DONE

--- redes-de-computadores/tp1/client.cpp
+++ redes-de-computadores/tp1/client.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
+#include <errno.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,50 +9,33 @@
 
 #include <vector>
 #include <iostream>
+#include <utility>
 
 
+// init active socket
 int init_socket(const char *ipaddr, const char *sck_port,
-              struct sockaddr_storage *sck_addr) {
-    if (ipaddr == NULL || sck_port == NULL) {
-        return -1;
-    }
+                sockaddr_storage *sck_addr);
 
-    uint16_t port = (uint16_t)atoi(sck_port);
-    if (port == 0) {
-        return -1;
-    }
-    port = htons(port);
+// check if the all characters in the word are be received
+bool word_is_complete(const std::vector<std::pair<char, bool>> &word);
 
-    struct in_addr inaddr4; // IPV4
-    if (inet_pton(AF_INET, ipaddr, &inaddr4)) {
-        struct sockaddr_in *addr4 = (struct sockaddr_in *)sck_addr;
-        addr4->sin_family = AF_INET;
-        addr4->sin_port = port;
-        addr4->sin_addr = inaddr4;
-        return 0;
-    }
+// read from stdin ignoring '\n' values
+char read_input();  
 
-    struct in6_addr inaddr6; // IPv6
-    if (inet_pton(AF_INET6, ipaddr, &inaddr6)) {
-        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)sck_addr;
-        addr6->sin6_family = AF_INET6;
-        addr6->sin6_port = port;
-        // a bit array must be copied
-        memcpy(&(addr6->sin6_addr), &inaddr6, sizeof(inaddr6));
-        return 0;
-    }
-
-    return -1;
-}
+// print response message and mark the sent character with a match flag
+void response_treatment(std::vector<std::pair<char, bool>> &word,
+    char *message, size_t length, char sent_char);
 
 int main(int argc, char *argv[]){
     char buffer[256];
+
+    std::vector<std::pair<char, bool>> word;
 
     if(argc != 3){
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_storage sck_addr;
+    sockaddr_storage sck_addr;
     if (0 != init_socket(argv[1], argv[2], &sck_addr)) {
         exit(EXIT_FAILURE);
     }
@@ -61,42 +45,63 @@ int main(int argc, char *argv[]){
     if (server_sck_id == -1) {
         exit(EXIT_FAILURE);
     }
-    struct sockaddr *addr = (struct sockaddr *)(&sck_addr);
+    sockaddr *addr = (sockaddr *)(&sck_addr);
     if (0 != connect(server_sck_id, addr, sizeof(sck_addr))) {
         exit(EXIT_FAILURE);
     }
     
+    // auxiliary attributes
     uint8_t msg_type = 0;
     std::vector<uint8_t> pack = std::vector<uint8_t>(2);
-    while(msg_type != 1){
-        if (0 > recv(server_sck_id, buffer, 2, MSG_WAITALL)){
+    int received_bytes;
+
+    // waiting for init message or game overs
+    while(msg_type != 1 && msg_type != 4){
+        received_bytes = recv(server_sck_id, buffer, 1, 0);
+        
+        if (0 > received_bytes){
             exit(EXIT_FAILURE);
         }
         msg_type = static_cast<uint8_t>(buffer[0]);
     }
-    std::cout << static_cast<int>(buffer[1]) << std::endl;
+    if(msg_type == 4) {
+        close(server_sck_id);
+        return 0;
+    }
+    
+    // The last recv() call does not use MSG_WAITALL
+    // Then it is possible that the message containins just 1 byte.
+    if(received_bytes==1){
+        recv(server_sck_id, (buffer+1), 1, MSG_WAITALL);
+    }
 
+    uint8_t word_length = static_cast<uint8_t>(buffer[1]);
+    int converted_length = static_cast<int>(word_length);
+    std::cout << converted_length << std::endl;
+
+    // creating data structure to compute the word
+    word = std::vector<std::pair<char, bool>>(converted_length);
+    for(int i=0; i<converted_length; i++){
+        word[i] = std::make_pair('?', false);
+    }
+    bool match_word = false;
 
     char ch;
-    while(true){
-        ch = '\n';
-        while(ch == '\n'){
-            ch = getchar();
-        }
+    while(!match_word){
+        ch = read_input();
+
         pack[0] = 2;
         pack[1] = static_cast<uint8_t>(ch);
 
         // send the message
-        if(0 > send(server_sck_id, reinterpret_cast<const char*>(pack.data()), 
+        if(0 > send(server_sck_id, reinterpret_cast<const char*>(pack.data()),
             pack.size(), 0)){
             exit(EXIT_FAILURE);
         }
 
-        std::cout << "send: " << pack[1] << std::endl;
-
-        //receiving message
+        // receiving message
+        // either letter response or game over
         msg_type = 0;
-        int received_bytes;
         while(msg_type != 3 && msg_type != 4){
             received_bytes = recv(server_sck_id, buffer, 2, 0);
             std::cout << static_cast<int>(buffer[1]) << std::endl;
@@ -117,15 +122,100 @@ int main(int argc, char *argv[]){
             msg_len = static_cast<size_t>(buffer[1]);
         }
         if(msg_len > 0){
-            if (0 > recv(server_sck_id, buffer, msg_len, MSG_WAITALL)){
+            // force the received data to be equals the msg_length received lastly
+            if (msg_len != recv(server_sck_id, buffer, msg_len, MSG_WAITALL)){
                 exit(EXIT_FAILURE);
             }
-            std::cout << static_cast<int>(buffer[0]);
-            for(int i=1; i<msg_len; i++){
-                std::cout << " " << static_cast<int>(buffer[i]);
-            }
-            std::cout << std::endl;
-        }
+            // compute word according server response
+            response_treatment(word, buffer, msg_len, ch);
 
+            // when the word is completely received, then
+            //   the loop will be stoped
+            match_word = word_is_complete(word);
+        }
+        std::cout << std::endl;
     }
+    // if the word is complete, then client will wait for
+    //  game over message
+    if(match_word){
+        while(msg_type != 4){
+            received_bytes = recv(server_sck_id, buffer, 1, 0);
+            msg_type = static_cast<uint8_t>(buffer[0]);
+        }
+    } else {
+        std::cout << std::endl;
+    }
+
+    std::cout << "Word: ";
+    for(auto it: word)
+        std::cout << it.first;
+    std::cout << std::endl;
+    close(server_sck_id);
+}
+
+int init_socket(const char *ipaddr, const char *sck_port,
+                sockaddr_storage *sck_addr) {
+    if (ipaddr == NULL || sck_port == NULL) {
+        return -1;
+    }
+
+    uint16_t port = (uint16_t)atoi(sck_port);
+    if (port == 0) {
+        return -1;
+    }
+    port = htons(port);
+
+    in_addr inaddr4; // IPV4
+    if (inet_pton(AF_INET, ipaddr, &inaddr4)) {
+        sockaddr_in *addr4 = (sockaddr_in *)sck_addr;
+        addr4->sin_family = AF_INET;
+        addr4->sin_port = port;
+        addr4->sin_addr = inaddr4;
+        return 0;
+    }
+
+    in6_addr inaddr6; // IPv6
+    if (inet_pton(AF_INET6, ipaddr, &inaddr6)) {
+        sockaddr_in6 *addr6 = (sockaddr_in6 *)sck_addr;
+        addr6->sin6_family = AF_INET6;
+        addr6->sin6_port = port;
+        // a bit array must be copied
+        memcpy(&(addr6->sin6_addr), &inaddr6, sizeof(inaddr6));
+        return 0;
+    }
+
+    return -1;
+}
+
+// check if the all characters in the word are be received
+bool word_is_complete(const std::vector<std::pair<char, bool>> &word){
+    for(auto i: word){
+        if (!i.second) return false;
+    }
+    return true;
+}
+
+// read from stdin ignoring '\n' values
+char read_input(){
+    char ch;
+    ch = '\n';
+    while(ch == '\n'){
+        ch = getchar();
+    }
+    return ch;
+}  
+
+void response_treatment(std::vector<std::pair<char, bool>> &word,
+    char *message, size_t length, char sent_char){
+    int index = static_cast<int>(message[0]);
+    word[index].first = sent_char;
+    word[index].second = true;
+    std::cout << index;
+    for(int i=1; i<length; i++){
+        index = static_cast<int>(message[i]);
+        word[index].first = sent_char;
+        word[index].second = true;
+        std::cout << " " << index;
+    }
+    std::cout << std::endl;
 }
--- redes-de-computadores/tp1/server.cpp
+++ redes-de-computadores/tp1/server.cpp
@@ -17,64 +17,25 @@ static const int _IPV4 = 4;
 static const int _IPV6 = 6;
 
 // Definitions
-static const int _IP_VERSION = _IPV4;
+static const int _IP_VERSION = _IPV6;
 static const std::string WORD = "beterraba";
 
+// init passive socket
+int init_socket(const char *sck_port, sockaddr_storage *sck_addr);
 
-int init_socket(const char *sck_port, sockaddr_storage *sck_addr){
-    uint16_t port = (uint16_t)atoi(sck_port); // unsigned short
-    if (port == 0) {
-        return -1;
-    }
-    port = htons(port); // host to network short
+// get all char repetitions in the word
+std::vector<uint8_t> get_repetitions(const std::string &word, char guessed);
 
-    memset(sck_addr, 0, sizeof(sockaddr_storage));
-    if (_IP_VERSION == _IPV4) {
-        sockaddr_in *addr4 = (sockaddr_in *)sck_addr;
-        addr4->sin_family = AF_INET;
-        addr4->sin_addr.s_addr = INADDR_ANY;
-        addr4->sin_port = port;
-        return 0;
-    } else if (_IP_VERSION == _IPV6) {
-        sockaddr_in6 *addr6 = (sockaddr_in6 *)sck_addr;
-        addr6->sin6_family = AF_INET6;
-        addr6->sin6_addr = in6addr_any;
-        addr6->sin6_port = port;
-        return 0;
-    } else {
-        return -1;
-    }
-}
+// check if all letters were informed
+bool word_is_complete(const std::map<char, bool> &word_map);
 
-std::vector<uint8_t> get_repetitions(const std::string &word, char guessed){
-    std::vector<uint8_t> result = std::vector<uint8_t>();
-    for(int i=0; i<word.size(); i++){
-        if(guessed == word[i]){
-            result.push_back(i);
-        }
-    }
-    return result;
-}
-
-bool word_is_complete(const std::map<char, bool> &word_map){
-    for(auto i: word_map){
-        if (!i.second) return false;
-    }
-    return true;
-}
-
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[]){    
     if(argc != 2){
         exit(EXIT_FAILURE);
     }
 
-
+    // data structure to manipulate the word
     std::string word = WORD;
-    std::map<char, bool> word_map = std::map<char, bool>();
-    for(char c: word){
-        word_map.insert(std::make_pair(c, false));
-    }
 
     sockaddr_storage sck_addr;
     if(0 != init_socket(argv[1], &sck_addr)){
@@ -102,74 +63,136 @@ int main(int argc, char *argv[]){
     sockaddr_storage cli_storage;
     sockaddr *cli_addr = (sockaddr *)(&cli_storage);
     socklen_t len = sizeof(cli_storage);
+
+    // auxiliary attributes
     std::vector<uint8_t> pack = std::vector<uint8_t>(2);
-    uint8_t *msg_tpe;
-
     char buffer[2];
-
-    // bool match_word = false;
+    bool match_word;
     uint8_t msg_type;
     char guessed;
-
-    int cliente_sck_id = accept(server_sck_id, cli_addr, &len);
-
-    // initial message
-    pack[0] = 1;
-    pack[1] = static_cast<uint8_t>(word.size());
-    if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
-            pack.size(), 0)){
-        exit(EXIT_FAILURE);
-    }
-
-    // while(!match_word){
-    while(true){
-        if (0 > recv(cliente_sck_id, buffer, 2, MSG_WAITALL)){
-            exit(EXIT_FAILURE);
-        }
-        msg_type = buffer[0]; 
-        guessed = buffer[1];
-        std::cout << "Received: " << buffer[1] << std::endl;
-
-        if(msg_type != 2) continue;
-
-        // find the guessed char in the word
-        // get the indexes of the char occurrences
-        // -- Create a vector<uint8_t> to get each index of the char in the word
-        
-        pack[0] = 3;
-        pack[1] = 0;
-        auto it = word_map.find(guessed);
-        bool match_char = it != word_map.end();
-
-        std::vector<uint8_t> char_repetitions;
-        if(match_char){
-            it->second = true;
-            if(word_is_complete(word_map)) break;
-
-            char_repetitions = get_repetitions(word, guessed);
-            pack[1] = static_cast<uint8_t>(char_repetitions.size());
-        }
-        if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
-            pack.size(), 0)){
-            exit(EXIT_FAILURE);
-        }
-        if(match_char){
-            if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(char_repetitions.data()), 
-                char_repetitions.size(), 0)){
-                exit(EXIT_FAILURE);
-            }
-        }
-        // match_word = word_is_complete(word_map);
-
-    }
-    pack[0] = 4;
-    if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
-        1, 0)){
-        exit(EXIT_FAILURE);
-    }
     
+    while(true) {
+        // waiting for a good connection
+        int cliente_sck_id;
+        do{
+            cliente_sck_id = accept(server_sck_id, cli_addr, &len);
+        } while(cliente_sck_id < 0);
 
-    close(cliente_sck_id);
+        std::map<char, bool> word_map = std::map<char, bool>();
+        for(char c: word){
+            word_map.insert(std::make_pair(c, false));
+        }
+        match_word = false;
 
+        // initial message
+        pack[0] = 1;
+        pack[1] = static_cast<uint8_t>(word.size());
+        if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
+                pack.size(), 0)){
+            close(cliente_sck_id);
+            continue;
+        }
 
+        // receiving guessed characters
+        bool _err = false;
+        while(!match_word){
+            if (0 >= recv(cliente_sck_id, buffer, 2, MSG_WAITALL)){
+                _err = true;
+                break;
+            }
+            msg_type = buffer[0];
+
+            // wait for the correct message code 
+            if(msg_type != 2) continue;
+
+            guessed = buffer[1];
+            
+            //send feeedback
+            pack[0] = 3;
+            pack[1] = 0;
+            auto it = word_map.find(guessed);
+            bool match_char = it != word_map.end();
+
+            std::vector<uint8_t> char_repetitions;
+            if(match_char){
+                it->second = true;
+
+                char_repetitions = get_repetitions(word, guessed);
+                pack[1] = static_cast<uint8_t>(char_repetitions.size());
+            }
+            // send quantity of repetitions of received char in the word
+            if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
+                pack.size(), 0)){
+                _err = true;
+                break;
+            }
+            // send indexes of the received char in the word
+            if(match_char){
+                if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(char_repetitions.data()), 
+                    char_repetitions.size(), 0)){
+                    _err = true;
+                    break;
+                }
+            }
+
+            match_word = word_is_complete(word_map);
+        }
+
+        if(_err) {
+            close(cliente_sck_id);
+            continue;
+        }
+        // send game over message
+        pack[0] = 4;
+        if(0 > send(cliente_sck_id, reinterpret_cast<const char*>(pack.data()), 
+            1, 0)){
+            close(cliente_sck_id);
+            continue;
+        }
+        close(cliente_sck_id);
+    }
+}
+
+int init_socket(const char *sck_port, sockaddr_storage *sck_addr){
+    uint16_t port = (uint16_t)atoi(sck_port); // unsigned short
+    if (port == 0) {
+        return -1;
+    }
+    port = htons(port); // host to network short
+
+    memset(sck_addr, 0, sizeof(sockaddr_storage));
+    if (_IP_VERSION == _IPV4) {
+        sockaddr_in *addr4 = (sockaddr_in *)sck_addr;
+        addr4->sin_family = AF_INET;
+        addr4->sin_addr.s_addr = INADDR_ANY;
+        addr4->sin_port = port;
+        return 0;
+    } else if (_IP_VERSION == _IPV6) {
+        sockaddr_in6 *addr6 = (sockaddr_in6 *)sck_addr;
+        addr6->sin6_family = AF_INET6;
+        addr6->sin6_addr = in6addr_any;
+        addr6->sin6_port = port;
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+// get all char repetitions in the word
+std::vector<uint8_t> get_repetitions(const std::string &word, char guessed){
+    std::vector<uint8_t> result = std::vector<uint8_t>();
+    for(int i=0; i<word.size(); i++){
+        if(guessed == word[i]){
+            result.push_back(i);
+        }
+    }
+    return result;
+}
+
+// check if all letters were informed
+bool word_is_complete(const std::map<char, bool> &word_map){
+    for(auto i: word_map){
+        if (!i.second) return false;
+    }
+    return true;
 }
