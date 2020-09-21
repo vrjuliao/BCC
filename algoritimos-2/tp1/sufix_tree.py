81f3fc7cb227aa7de7a75154939d698b01d46486
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-09-20 21:15:24 -0300

    alg2(tp1): fixing the dfs algorithm to comupte the biggest sub string which repeats in the text

--- algoritimos-2/tp1/sufix_tree.py
+++ algoritimos-2/tp1/sufix_tree.py
@@ -20,6 +20,10 @@ class Node:
     self.__childrens.append(node)
 
   @property
+  def length(self):
+    return self.__sufix[1] - self.__sufix[0] + 1
+
+  @property
   def sufix(self):
     return self.__sufix
 
@@ -48,15 +52,16 @@ class SufixTree:
 
   def __create_tree(self, text):
     right, left = 0, 0
-    # pdb.set_trace()
-    for i in range(len(text)-1, -1, -1):
+    for current_index in range(len(text)-1, -1, -1):
       current_node = self.__root
-      left = i
+      left = current_index
       right = len(text)-1
       child, child_index = current_node.find_children(text, left, right)
-      # create a new node with indexes being the difference between current_node.left
+
       create_new_node = True
       while(child is not None):
+        
+        # spilt sufix
         new_substr, new_child_substr = self.__difference_sufixes(child.sufix[0],
           child.sufix[1], left, right)
 
@@ -64,21 +69,23 @@ class SufixTree:
         new_sufix_len = new_child_substr[1] - new_child_substr[0]
         child_sufix_len = child.sufix[1] - child.sufix[0]
         if(new_sufix_len == child_sufix_len):
+          # The analyzed sufix is son of 'child'
+          # All characters of child were matched in analyzed sufix
+          # Change the analized sufix indexes, to find a new son
           child.sufix = new_child_substr
           current_node = child
           left, right = new_substr
           child, child_index = current_node.find_children(text, left, right)
-          # 1 - reestart loop changing left and right values, and getting a new 
-          #     child node
+          # reestart loop changing left and right values, and getting a new 
         else:
-          # 1 - create a new_child = child
-          # 2 - set the sufix of new_child = the difference of the current sufix
-          #     and t
+          # Analyzed sufix has not a complete match with the 'child' nod 
+          # create a new_child = child
+          # set the sufix of new_child = the difference of the current sufix and t
           new_child = Node(new_child_substr[0], new_child_substr[1], False)
           child.sufix = (child.sufix[0]+new_sufix_len+1, child.sufix[1])
           new_child.new_children(Node(new_substr[0], new_substr[1], True))
           new_child.new_children(child)
-          # 3 - the old child will be a children of new_child
+          # the old child will be a children of new_child
           current_node.set_children(new_child, child_index)
           create_new_node = False
           break
@@ -90,68 +97,61 @@ class SufixTree:
     new_left = mleft
     
     for i in range(nleft, nright+1, 1):
-
       if(self.__text[i] != self.__text[mleft]):
         break
-        # return (mleft, mright), (new_left, mleft-1)
       else:
         mleft += 1
     return (mleft, mright), (new_left, mleft-1)
     
+  def __change_substr(self, substr_data, children_data):
+    repetitions, indexes = children_data
+    child_size = indexes[1] - indexes[0] + 1
+    substr_size = substr_data[1][1] - substr_data[1][0] + 1
+
+    if (child_size > substr_size and repetitions > 1) or \
+      (child_size == substr_size and repetitions > substr_data[0]):
+      return True
+    
+    return False
+
   def biggets_substr(self):
     # substr_data = (<repetitions>, <(left, right)>)
-    substr_data = (0, (-1, -1))
-    substr_size = 0
+    substr_data = (0, (0, -1))
     for child in self.__root.childrens:
-      repetitions, indexes = self.__dfs_for_biggest_susbtr(child)
-      child_size = indexes[1] - indexes[0] + 1
-      if child_size > substr_size:
-        if repetitions > 1:
-          substr_size = child_size
-          substr_data = repetitions, indexes
-      elif child_size == substr_size:
-        if repetitions > substr_data[0]:
-          substr_size = child_size
-          substr_data = repetitions, indexes
+      children_data = self.__dfs_for_biggest_susbtr(child)
+      if(self.__change_substr(substr_data, children_data) is True):
+        substr_data = children_data
      
     return substr_data
 
   def __dfs_for_biggest_susbtr(self, parent):
     childrens_qtt = len(parent.childrens)
-    substr_data = (0,(0,0)) # (0, (0,0))
-    substr_size = 0 # 0
+    substr_data = (0,(0 ,-1)) # (0, (0,0))
     if childrens_qtt == 0:
       return (0, (0, 0))
 
     for child in parent.childrens:
-      repetitions, indexes = self.__dfs_for_biggest_susbtr(child)
-      size = indexes[1] - indexes[0] + 1
-      if size > substr_size:
-        if repetitions > 1:
-          substr_data = (repetitions, indexes)
-          substr_size = size
-      elif size == substr_size:
-        if repetitions > substr_data[0]:
-          substr_data = (repetitions, indexes)
-          substr_size = size
+      children_data = self.__dfs_for_biggest_susbtr(child)
+      if(self.__change_substr(substr_data, children_data) is True):
+        substr_data = children_data
 
     if substr_data[0] == 0:
+
+      if parent.end_of_string:
+        childrens_qtt += 1
+      
       substr_data = (childrens_qtt, parent.sufix)
     else:
       size = parent.sufix[1]-parent.sufix[0]+1
       indexes = substr_data[1]
       substr_data = (substr_data[0], (indexes[0] - size, indexes[1]))
     
-    if parent.end_of_string:
-      substr_data = (substr_data[0]+1, substr_data[1])
-
     return substr_data
   
   def graph_to_mermaid(self):
     result = ""
     for child in self.__root.childrens :
       child_name = self.__text[child.sufix[0]: child.sufix[1]+1]
-      # child_name = ("(%d, %d)" % (child.sufix[0], child.sufix[1]))
       result += "Root --> " + child_name + "\n"
       result += self.__dfs_to_print(child_name, child)
     return result
@@ -160,17 +160,16 @@ class SufixTree:
     s = ""
     for child in node.childrens:
       child_name = self.__text[child.sufix[0]: child.sufix[1]+1]
-      # child_name = ("(%d, %d)" % (child.sufix[0], child.sufix[1]))
       s += node_name + " --> " + child_name + "\n"
       s += self.__dfs_to_print(child_name, child)
     return s
 
 def main():
-    text = "aaaaaaaaaaaaaaaaaaaaaaaaaaabbb"
+    text = "abcdefgabcdeiskaabcde"
     tree = SufixTree(text)
     
     repetitions, indexes = tree.biggets_substr()
-    # print(tree.graph_to_mermaid())
+    print(tree.graph_to_mermaid())
     print(repetitions)
     print(text[indexes[0]: indexes[1]+1])
 
