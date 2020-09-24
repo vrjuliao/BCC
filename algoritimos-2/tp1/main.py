fc6c343fe4b24b9d8ac09337924153d10cd3f503
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-09-24 14:00:25 -0300

    alg2(tp1): DONE

--- algoritimos-2/tp1/main.py
+++ algoritimos-2/tp1/main.py
@@ -9,9 +9,12 @@ def main(argv):
     splited_text = f_open.readlines()
     separator = ""
     text = separator.join([s[:-1] for s in splited_text])
+    # computing sufixes and create the tree
     tree = sufix_tree.SufixTree(text)
-    
+
+    # get the biggest substring that repeats in the text
     repetitions, indexes = tree.biggest_substr()
+    
     print(repetitions)
     print(text[indexes[0]: indexes[1]+1])
 
