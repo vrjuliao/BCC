04a964fcfb38b8ecfaa33d5538c4a008c369624a
V. Julião <viniciusjuliao@dcc.ufmg.br>
2021-07-25 02:07:05 -0300

    PO(tp1): all procedurals implemented - missing printing step

--- pesquisa-operacional/tp1/main.py
+++ pesquisa-operacional/tp1/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.twodim_base import _min_int
 
 class Tableau:
   self.__n = 0
@@ -35,45 +36,82 @@ class Tableau:
     
     return tb
 
-  # return -1 if there is no pivot to choose
-  def __get_pivot_index(self, vecC):
+  # return -1 if there is no column to choose
+  def __get_pivot_column_index(self, vecC):
     for i in range(len(vecC)):
       if round(vecC[i], 5) < 0:
         return i
       return -1
 
+  # return -1 if there is no pivot to choose -> it means unlimited
+  def __get_pivot_row_index(self, column, b):
+    min_idx = -1
+    min_val = -1
+    for i in range(self.__m):
+      if round(column[i], 5) > 0:
+        factor = round(column[i]/b[i], 5)
+        if min_idx == -1 or factor < min_val:
+          min_idx = i
+          min_val = factor
+    
+    return min_idx
+
+  def __escalonar(self, pcol, prow, tb):
+    tb[prow, :] = np.round(tb[prow, :] * (1/tb[prow, pcol]), 5)
+
+    for i in range(self.__m):
+      if i == prow:
+        continue
+      factor = -tb[i, pcol]
+      tb[i, :] += (factor*tb[prow,:])
+
   def __solve_tableau(self, tb):
     # 1 -  pegar o indice da coluna que será pivoteada usando a regra de Bland
-    pivot = self.__get_pivot_index(tb[0, self.__n:-1])
+    pcol = self.__get_pivot_column_index(tb[0, self.__n:-1])
 
-    while pivot != -1:
+    while pcol != -1:
       # 1 - escolher qual a linha ideal para o pivot segundo Bland
+      pcol += self.__n
+      prow = self.__get_pivot_row_index(tb[1:, pcol], tb[1:, -1])
+      if(prow == -1):
+        #ilimitado
+        pass
+      prow += 1
       # 2 - escalonar a matriz
+      self.__escalonar(pcol, prow, tb)
+      
       # 3 - pegar o novo pivot
-      pass
+      pcol = self.__get_pivot_column_index(tb[0, self.__n:-1])
 
     return tb
 
   def solve(self):
-    # solve the auxiliar
-    table = []
     #create the auxiliar tableau
-    tb = self.__create_aux_tableau()
-    tb = self.__solve_tableau(tb)
+    tbAux = self.__create_aux_tableau()
+    # solve the auxiliar
+    tbAux = self.__solve_tableau(tbAux)
     
-    if round(table[0][-1], 5 < 0):
+    if round(tbAux[0][-1], 5 < 0):
       # inviável - printar o certificado (self.__y)
       return
 
-    # copiar remontar o tableau:
-    #   1 - remover a parte auxiliar da matrix A
-    #   2 - Colocar o vetor minus_C no cabeçalho
-    for i in range(self.__n):
-      for j in range(self.__m):
-        self.__A[i][j] = auxA[i][j]
+    # remover a parte auxiliar da matrix A
+    tb = np.array((self.__n+1, self.__n+self.__m+1))
+    tb[0, :self.__n] = tbAux[0, :self.__n] # y
+    tb[1:, :self.__n] = tbAux[1:, :self.__n] # vero
+    tb[1:, self.__n:self.__n+self.__m] = tbAux[1:, self.__n:self.__n+self.__m] # A
+    tb[1:, -1] = tbAux[1:, -1] # b
+    
+    # Colocar o vetor minus_C no cabeçalho
+    tb[0, self.__n:self.__n+self.__m] = -tbAux[0, self.__n:self.__n+self.__m] # -C
 
-    #TODO: resolver o tableau remontado
+    #resolver o tableau remontado
+    tb = self.__solve_tableau(tb)
     # extrair as informações do tableu para as variáveis corretas
+    self.__y = tbAux[0, :self.__n]
+    self.__vero = tbAux[1:, :self.__n]
+    self.__A = tbAux[1:, self.__n:self.__n+self.__m]
+    self.__b = tbAux[1:, -1]
 
       
 def main():
